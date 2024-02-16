bf4f79c4f3a9433aea047ee016c207b62400891d
Amardeep Pawar <67967820+Amardeep01-ytc@users.noreply.github.com>
2024-02-16 16:54:11 +0530

    Update Camera-Final

--- 09-cameras-final/src/script.js
+++ 09-cameras-final/src/script.js
@@ -73,4 +73,43 @@ const tick = () =>
 
 tick()
 
+     
+
+/*
+  Three js Bulid-in Controls : 
+    1. Device Orientation Controls: 
+        -Device Orientation control automatically retrive the device orientation if your device is OS and browser allow it and  rotate the camera acccordingly 
+        -Useful it to create immersive universe or VR experiences
+    
+    2. Fly controls :  
+        -Fly controls enable to moving a camera like if you were on a spaceship 
+        -You can rotate on all 3 axes, go forward and go backword
+    
+    3. First-Person control :
+        - First-person control is like fly control, but with fix up axis 
+        - Doesn't work like in 'FPS' Games.
+    
+    4. Pointer-Lock Controls :
+        - Pointer-lock control is uses the pointer lock JavaScript API
+        - Hard to use and almost only handles the pointer lock and camera rotation
+
+    5. Orbit Controls :
+        - Orbit Controls similar to the controls we made with more features
+
+    6. Track-ball Controls :
+        - Track-ball control is like orbit control without the vertical angle limit
+    
+    7. Transfrom Control :
+        - Transform control has nothing to do with camera
+
+    8. Drag Control : 
+        - Drag control is same as Transform controls it will also noting to do with camera
+
+         
+    When to use controls :
+    - Controls are handy, but they have limitations 
+    - If you want to use those, make sure that they support all the features you need
+    - If not, you'll have to do it at your own
+   
+*/
 
