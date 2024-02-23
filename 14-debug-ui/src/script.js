a355ad3f8a191f1978e1532786137e0b1bc09eec
Amardeep Pawar <67967820+Amardeep01-ytc@users.noreply.github.com>
2024-02-23 22:30:04 +0530

    Debug Notes Added

--- 14-debug-ui/src/script.js
+++ 14-debug-ui/src/script.js
@@ -125,3 +125,139 @@ const tick = () =>
 }
 
 tick()
+
+
+/*
+Debug-UI -->
+    - we need to be able to tweak and debug easily
+    - It concers the developer,the designer and even the client
+    - It will help finding the perfect color, speed, quentity, dimension, diection, timing, etc.
+
+What we need a "debug UI" -->
+    - we ca create our own or we can use a library
+    - dat.GUI
+    - control-panel
+    - ControlKit
+    - Guify
+    - Oui
+In this lesson we are going to use dat.GUI but you should try the other ones
+
+SetUP -->
+    It is same as before with the red cube and OrbitControls
+
+How to implement dat.GUI -->
+    - we need to add the fat.gui dependency like we added the gsap dependency 
+    - In the terminal run 'npm install --save dat.gui'
+       and import it with 
+      import * as dat from 'dat.gui'
+
+We can now instantiate it
+//debug
+    const gui = new dat.GUI()
+
+There are different types of elemets you can add to that panel 
+- Range = for numbers with minimum and maximum value
+- Color = for colors with various formats
+- Text  = for simple texts
+- Checkbox = for booleans(true or false)
+- Select = for a choice from a list of values(like mcq)
+- Button = to trigger functions
+- Folder = to organize your panel if you have too many elements
+
+Add Elements -->
+    - Use gui.add(...) to add an element (a tweak)
+    - the first parameter is an object 
+    - the second parameter is the property you want to tweak(element)
+      gui.add(mesh.position, 'y')
+    - The next parameters handle 
+        minimum
+        maximum
+        step(or precision)
+      gui.add(mesh.position, 'y', -3, 3, 0.01)
+    It will up to only these values 
+    
+    - we can also use the min(..).max(..)and step(..) methods
+      gui.add(mesh.position, 'y').min(-3).max(3).step(0.01)
+    It will give same result as before
+
+    -we can also write like this:
+    gui
+    .add(mesh.position, 'y')
+    .min(-3)
+    .max(3)
+    .step(0.01)
+
+    -we can change the label with the name(..)method
+    gui
+    .add(mesh.position, 'y')
+    .min(-3)
+    .max(3)
+    .step(0.01)
+    .name('elevation')
+
+    -Dat.GUI will change the type of tweak according to the type of property 
+    -Add a tweak for the visible property
+       gui.add(mesh, 'visible')
+
+    - We can do the same with the wireframe proprty of the material 
+       gui.add(material, 'wireframe')
+
+COLORS :
+We can Change color of the cube also -->
+    - we need to use addColor(...) beacuse Dat.GUI cannot guess that it's not a simple string (eg. '0xff0000' It is only string)
+    - we have to create a temporary object in order to have an accessible property
+    - Create parameters variable at the start of your code  
+        const parameters = {
+            color: 0xff0000
+        }
+    - Then use the addColor(...) method 
+        gui.addColor(parameters, 'color')
+    - Changing the color doesn't affect the material 
+      We need to apply the parameters.color on the material when the color changed
+    - we can use onChnage(...) method to know when the tweak value chnaged and 'material.color.set(..)' to update the color
+      because 'material.color' is an instance of Color
+        const gui = new dat.GUI()
+
+        const parameters ={
+           color: 0xff0000
+        }
+
+        gui
+           .addColor(parameters, 'color')
+           .onChange(() =>
+           {
+               material.color.set(parameters.color)
+           })
+
+    - Insted of specifying 0xff0000 in the parameters and in the material,we can use the parameter.color on the material 
+        const material = new THREE.MeshBasic.Material({ color : parameters.color })
+
+FUNCTION :
+    - To trigger a function, we need to store it in object 
+    - we can use the parameters object again and create a spin method
+        const parameters ={
+         color: 0xff0000,
+          spin : () =>
+         {
+            gsap.to(mesh.rotation, { duration: 1, y: mesh.rotation.y + 10})
+        }
+       }
+    - and again ,we can add the tweak to our gui
+        gui.add(parameters, 'spin')
+
+TIPS :
+    HIDE
+       -Press H to hide the panel 
+       - and if you want the panel to be hidden at start, use gui.hide()
+
+    Colse
+       - close the panel by clicking on its bottom part
+       - if you want to be closed at start, send an object when instantiating and pass it 'closed : true'
+            const gui = new dat.GUI({ closed : true })
+     
+    Width 
+       - you can drag and drop the panel to change its width 
+       - you can change the default width with 'width:...'
+            const gui = new dat.GUI({ width : 400 })
+
+*/
