0097a0df1e929f8fea9dfad0e121fded077d1ac1
Amardeep Pawar <67967820+Amardeep01-ytc@users.noreply.github.com>
2024-03-08 22:34:31 +0530

    Update script.js of textures

--- 17-textures-final/src/script.js
+++ 17-textures-final/src/script.js
@@ -145,5 +145,69 @@ const tick = () =>
     window.requestAnimationFrame(tick)
 }
 
+/*
+OFFSET :
+    - We can offset the texture using the offset property which is Vector2
+        colorTexture.offset.x = 0.5
+        colorTexture.offset.y = 0.5
+
+Rotation :
+    - We can rotate the texture using the rotation property 
+        colorTexture.rotation = Math.PI * 0.25
+    - Remove the offset and repeat properties to notice that the rotation occurs around between left corner
+    - that is 0, 0 UV coordinates
+    - We can change this pivot point with the center property which is Vector2
+        colorTexture.rotation = Math.PI * 0.25
+        colorTexture.center.x = o.5
+        colorTexture.center.y = o.5
+
+Filtering and Mipmapping :
+    - If you look at the cube's top face while this face is almost hideen, you'll see a blury texture
+    - that is due to the filtering and mipmapping
+    - Mip Mapping is a technic that consist of creating half a smaller version of a texture again and agian until we get a 1*1 texture.
+    - All those texture variations are sent to the GPU, and the GPU will choose the most appropriate version of the texture
+    - All of this is already handeled by Three.js and the GPU but we can choose different algorithms.
+      There are two types of filter algorithms
+    1. Minification Filter :
+        - Happens when the pixels of texture are smaller than the pixels of the render
+        - In other words, the texture is too big for the surface, it covers
+        - We can chage the minification filter of the texture using minFilter property with those 6 values 
+            1. THREE.NearestFilter
+            2. THREE.LinearFilter
+            3. THREE.NearestMipmapNearestFilter
+            4. THREE.NearestMipmapLinearFilter
+            5. THREE.LinearMipmapNearestFilter
+            6. THREE.LinearMipmapLinearFilter(default)
+    - We will only test THREE.NearestFilter
+        colorTexture.minFilter = THREE.NearestFilter
+    The pixels are more sharp but also glitchy  
+    - We can test with checkerboard-1024x1024.png to see those glichy artfacts more clearly 
+        const colorTexture = textureLoader.load('/textures/checkerboard-1024x1024.png')
+      This is called moire patterns
+
+Magnification Filter :
+    - Happens when the pixels of the texture are bigger than the pixels of the render
+    - In other words, the texture is too small for the surface it covers 
+    - Use the checkerboard-8x8.png texture
+        const colorTexture  = textureLoader.load('/texture/checkerboard-8x8.png')
+    - It look bad, but it is depends on the context
+    - In the effect isn't exaggerated, the users will probably not noticed
+    - Solution : We can change the magnification filter of the texture using the magFilter property with those 2 values 
+        * THREE.NearestFilter 
+        * THREE.LinearFilter(Default)
+    - Useful if you're going for a Minecraft style 
+        Use the minecraft.png texture
+        const colorTexture = textLoader.load('/texture/minecraft.png')
+
+    - THREE.NearestFilter is cheper than the other ones and if the result is fine with you just use it
+      We can get framerate using NearestFilter 
+
+    - If we are using THREE.NearestFilter on minFilter, we dont need the mipmaps
+    - we can deactivate the mipmaps generating with colorTexture.generateMipmaps = false
+        colorTexture.generatingMipmaps = false
+        colorTexture.minFilter = THREE.Nearest Filter 
+
+
 tick()
 //This is the final topic of textures 
+*/
